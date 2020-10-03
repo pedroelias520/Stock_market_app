e043e54df85d2ddc1f18d4ddab9aeb3a44a3644b
Pedro Elias <epedro520@gmail.com>
2020-10-02 21:52:12 -0300

    upgrade:front-end upgdated

---
+++ app/src/main/java/com/example/stock_market/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stock_market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
